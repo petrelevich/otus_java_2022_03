1e155752f8066e97d022882ba71e03d75041643a
alexamikaya <alexamikaya@gmail.com>
2022-06-13 10:26:28 +0300

    Сборщик мусора, оптимизация кода и определение оптимального размера хипа

--- L08-gc/homework/src/main/java/ru/calculator/Summator.java
+++ L08-gc/homework/src/main/java/ru/calculator/Summator.java
@@ -3,18 +3,19 @@ package ru.calculator;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Summator {
-    private int sum = 0;
-    private int prevValue = 0;
-    private int prevPrevValue = 0;
-    private int sumLastThreeValues = 0;
-    private int someValue = 0;
+    private Integer sum = 0;
+    private Integer prevValue = 0;
+    private Integer prevPrevValue = 0;
+    private Integer sumLastThreeValues = 0;
+    private Integer someValue = 0;
     private final List<Data> listValues = new ArrayList<>();
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
         listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
+        if (listValues.size() == 6_600_000) {
             listValues.clear();
         }
         sum += data.getValue();
@@ -30,23 +31,23 @@ public class Summator {
         }
     }
 
-    public int getSum() {
+    public Integer getSum() {
         return sum;
     }
 
-    public int getPrevValue() {
+    public Integer getPrevValue() {
         return prevValue;
     }
 
-    public int getPrevPrevValue() {
+    public Integer getPrevPrevValue() {
         return prevPrevValue;
     }
 
-    public int getSumLastThreeValues() {
+    public Integer getSumLastThreeValues() {
         return sumLastThreeValues;
     }
 
-    public int getSomeValue() {
+    public Integer getSomeValue() {
         return someValue;
     }
 }
