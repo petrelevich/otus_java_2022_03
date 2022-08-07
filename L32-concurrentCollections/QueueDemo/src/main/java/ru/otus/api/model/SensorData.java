faa4807176edb33bd7b321bc55fa75350978fa93
stvort <stvort@mail.ru>
2022-08-07 19:08:54 +0400

    L32-concurrentCollections added

---
+++ L32-concurrentCollections/QueueDemo/src/main/java/ru/otus/api/model/SensorData.java
@@ -0,0 +1,28 @@
+package ru.otus.api.model;
+
+
+import java.time.LocalDateTime;
+
+public class SensorData {
+    private final LocalDateTime measurementTime;
+    private final String room;
+    private final Double value;
+
+    public SensorData(LocalDateTime measurementTime, String room, Double value) {
+        this.measurementTime = measurementTime;
+        this.room = room;
+        this.value = value;
+    }
+
+    public LocalDateTime getMeasurementTime() {
+        return measurementTime;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+}
