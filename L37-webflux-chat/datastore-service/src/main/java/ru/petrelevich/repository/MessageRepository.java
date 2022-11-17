d99be8fb112da65f7ae1129a41689c279872a65e
sergey <petrelevich@yandex.ru>
2022-11-17 09:54:42 +0300

    L37 fixed annotation

--- L37-webflux-chat/datastore-service/src/main/java/ru/petrelevich/repository/MessageRepository.java
+++ L37-webflux-chat/datastore-service/src/main/java/ru/petrelevich/repository/MessageRepository.java
@@ -1,6 +1,6 @@
 package ru.petrelevich.repository;
 
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
